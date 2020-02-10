5b1426d8cda3bdc36b4c1a8891510f1348f314a9
jathi <siva.kaddala1998@gmail.com>
2020-02-10 19:24:43 +0530

    calculator

---
+++ Calculator/src/main/java/Simple/Calculator/Division.java
@@ -0,0 +1,14 @@
+package Simple.Calculator;
+
+public class Division {
+	double x,y;
+	public Division(double x,double y) {
+		// TODO Auto-generated constructor stub
+		this.x=x;
+		this.y=y;
+	}
+	void div() {
+		System.out.println(this.x/this.y);
+	}
+
+}
